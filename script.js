99249b3be7c0eb13ad94a99a3ee0ad526fb29292
caio-glind <77419047+caio-glind@users.noreply.github.com>
2021-11-21 22:25:32 -0500

    add os card de produtos e junto os islaides do banner

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 //contote do baner para lola para os lados por meiio do escrol do molse 
-document.querySelector("#items")
+/*document.querySelector("#items")
 .addEventListener("wheel", event =>{
   if(event.deltaY > 0){
       event.target.scrollBy(300,0)
